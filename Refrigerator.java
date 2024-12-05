cfef16bde991a1b2dfc0f5775f91f07c39c3a186
alfredscyriac <alfredscyriac@gmail.com>
2024-12-05 17:42:29 -0500

    Add Refrigerator.java

---
+++ Refrigerator.java
@@ -0,0 +1,31 @@
+/**
+ * Refrigerator extends the Appliance.java class as it a specific type of appliance
+ * The unique variable to the refrigerator class is its cubic feet
+ */
+public class Refrigerator extends Appliance {
+    private int cubicFeet;
+    private double price;
+
+     /**
+     * Constructor for a refrigerator object
+     * @param serialNumber is the serial number of the refrigerator
+     * @param price is the price of the refrigerator
+     * @param cubicFeet is the cubic feet of the refrigerator 
+     * All passed in parameters set the values for the private instance 
+     * variables while retriving serialNumber from the parent class which is appliance
+     */
+    public Refrigerator(String serialNumber, double price, int cubicFeet) {
+        super(serialNumber);
+        this.price = price;
+        this.cubicFeet = cubicFeet;
+    }
+
+    /** 
+     * @return a string that concatenates all the components of the refrigerator class for our GUI
+     * (serial number, price, cubic feet)
+     */
+    @Override
+    public String toString() {
+        return "Serial=" + getSerialNumber() + ". Price=$" + price + ". Cubic Feet=" + cubicFeet + ".";
+    }
+}
