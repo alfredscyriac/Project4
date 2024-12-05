181eb6164e2cdd3f5d3618aa8df00353d8d1690b
alfredscyriac <alfredscyriac@gmail.com>
2024-12-05 18:16:24 -0500

    Check if serial number is valid then use put method to add it to the approperiate TreeMap

--- ApplianceComparator.java
+++ ApplianceComparator.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class ApplianceComparator implements Comparator<String> {
     @Override
     public int compare(String serial1, String serial2) {
-        return serial1.compareTo(serial2); // Lexicographical ordering
+        return serial1.compareTo(serial2); 
     }
 }
--- Project4.java
+++ Project4.java
@@ -2,8 +2,8 @@ import java.util.TreeMap;
 public class Project4 {
     public static void main(String[] args) {
         String filePath = args[0];
-        TreeMap<String, Refrigerator> refrigerators = new TreeMap<>(new ApplianceComparator());
-        TreeMap<String, Dishwasher> dishwashers = new TreeMap<>(new ApplianceComparator());
+        TreeMap<Appliance, Refrigerator> refrigerators = new TreeMap<>(new ApplianceComparator());
+        TreeMap<Appliance, Dishwasher> dishwashers = new TreeMap<>(new ApplianceComparator());
         TreeMap<String, Microwave> microwaves = new TreeMap<>(new ApplianceComparator());
         try{
             TextFileInput fileInput = new TextFileInput(filePath);
@@ -14,6 +14,20 @@ public class Project4 {
 
                 String serial = parts[0];
                 double price = Double.parseDouble(parts[1]);
+                if (Appliance.isValid(serial)) {
+                    if (serial.charAt(0) == 'R') {
+                        int cubicFeet = Integer.parseInt(parts[2]);
+                        refrigerators.put(serial, new Refrigerator(serial, price, cubicFeet));
+                    } else if (serial.charAt(0) == 'M') {
+                        int watts = Integer.parseInt(parts[2]);
+                        microwaves.put(serial, new Microwave(serial, price, watts));
+                    } else if (serial.charAt(0) == 'D') {
+                        boolean undercounterInstallation = parts[2].equalsIgnoreCase("Y");
+                        dishwashers.put(serial, new Dishwasher(serial, price, undercounterInstallation));
+                    }
+                }
+            }
+            fileInput.close();
             }
         }
         catch(Exception e){
