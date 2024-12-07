eef662344f9119eedc870818e8079f5d74fe4ecf
alfredscyriac <alfredscyriac@gmail.com>
2024-12-06 22:25:06 -0500

    Create private instance variables for ApplianceGUI which include the 3 TreeMaps and 3 JTextAreas

--- ApplianceGUI.java
+++ ApplianceGUI.java
@@ -3,5 +3,11 @@ import java.awt.*;
 import java.util.TreeMap;
 
 public class ApplianceGUI {
+    private TreeMap<String, Refrigerator> refrigerators; 
+    private TreeMap<String, Dishwasher> dishwashers; 
+    private TreeMap<String, Microwave> microwaves; 
+    private JTextArea fridgeArea;
+    private JTextArea dishWasherArea;
+    private JTextArea microwaveArea;
     
 }
