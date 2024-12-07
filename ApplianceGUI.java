5fe4e9d0f7209e67d00b82ae44c24746ef41d7ee
alfredscyriac <alfredscyriac@gmail.com>
2024-12-06 22:29:02 -0500

    Add ApplianceGUI constructor from Project3 but add function searchAppliances() with an ActionListener to allow users to search for appliances

--- ApplianceGUI.java
+++ ApplianceGUI.java
@@ -9,5 +9,36 @@ public class ApplianceGUI {
     private JTextArea fridgeArea;
     private JTextArea dishWasherArea;
     private JTextArea microwaveArea;
+    public ApplianceGUI(TreeMap<String, Refrigerator> refrigerators,
+                        TreeMap<String, Dishwasher> dishwashers,
+                        TreeMap<String, Microwave> microwaves) {
+        this.refrigerators = refrigerators;
+        this.dishwashers = dishwashers;
+        this.microwaves = microwaves;
+        JFrame frame = new JFrame("Project4 -- Appliance Management with TreeMap");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        fridgeArea = new JTextArea();
+        dishWasherArea = new JTextArea();
+        microwaveArea = new JTextArea();
+
+        JPanel appliancePanel = new JPanel(new GridLayout(1, 3));
+        appliancePanel.add(new JScrollPane(fridgeArea)); 
+        appliancePanel.add(new JScrollPane(dishWasherArea)); 
+        appliancePanel.add(new JScrollPane(microwaveArea)); 
+
+        JButton searchButton = new JButton("Search");
+        searchButton.addActionListener(e -> searchAppliances());
+
+
+        frame.add(appliancePanel, BorderLayout.CENTER);
+        frame.add(searchButton, BorderLayout.SOUTH);
+
+        frame.setSize(900, 400);
+        frame.setVisible(true);
+
+        updateDisplay();
+    }
     
 }
