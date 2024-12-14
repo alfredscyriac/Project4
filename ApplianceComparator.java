85967c7e9446f2b53a2dd6e18e15a806291ae3de
alfredscyriac <alfredscyriac@gmail.com>
2024-12-13 23:21:35 -0500

    Add javadoc for ApplianceComparator.java

--- ApplianceComparator.java
+++ ApplianceComparator.java
@@ -1,6 +1,13 @@
 import java.util.Comparator;
-
 public class ApplianceComparator implements Comparator<String> {
+    /**
+     * Compares two serial numbers to sort their ordering in the TreeMap
+     * @param serial1 The first serial number to be compared
+     * @param serial2 The second serial number to be compared
+     * @return A negative integer if serial1 is less than serial2,
+     * a positive integer if serial1 is greater than serial2,
+     * or 0 if serial1 and serial2 are equal
+     */
     @Override
     public int compare(String serial1, String serial2) {
         return serial1.compareTo(serial2); 
